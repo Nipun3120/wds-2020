7e410edf3381c8e94c54248dc1bbbf30bbfc5f17
Rahul Goyal <rahul@Rahul>
2020-09-22 10:46:54 -0400

    Login and Register pages

--- wds/core/forms.py
+++ wds/core/forms.py
@@ -1,6 +1,11 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import *
 
-class RegisterForm(forms.Form):
-    teamname = forms.CharField(widget=forms.TextInput(), required=True, max_length=50)
-    email = forms.EmailField(label='E-mail', required=True, max_length=50)
-    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+class RegisterForm(forms.ModelForm):
+    password=forms.CharField(widget=forms.PasswordInput)
+
+    class Meta():
+        model = User
+        fields = ('username','email','password')
+
--- wds/wds/settings.py
+++ wds/wds/settings.py
@@ -124,3 +124,4 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+LOGIN_REDIRECT_URL = 'home'
