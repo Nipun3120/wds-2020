3183996587000426ec409c8a8b20c07c65034bb2
Rahul Goyal <rahul@rahul.org>
2020-10-02 20:47:26 -0400

    Stock Section

--- wds/core/models.py
+++ wds/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Stock(models.Model):
+    StockName = models.CharField(blank=True, max_length=100)
+    CurrentPrice = models.FloatField(null=True,blank=True)
+    def __str__(self):
+        return self.StockName
--- wds/core/urls.py
+++ wds/core/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('userlogin/',views.user_login,name='userlogin'),
     path('userlogout/',views.userlogout,name='userlogout'),
     path('dashboard/',views.dashboard,name='dashboard'),
+    path('stocks/',views.stocks,name='stocks'),
     path('',home,name="home"),
     
 ]
