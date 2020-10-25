c0bc8b6684f3c54936dd19198360951de464cd41
Rahul Goyal <rahul@rahul.org>
2020-10-24 20:58:14 -0400

    Updated model.py

--- wds/core/models.py
+++ wds/core/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
+
+from notification.models import Notification
+from chat.utils import find_or_create_private_chat
 # Create your models here.
 stock_list=(
     ('stock1','stock1'),
@@ -46,6 +52,32 @@ def create_stock(sender,instance,created,**kwargs):
 
 post_save.connect(create_stock,sender=User)
 
+class TradeList(models.Model):
+    user=models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user")
+    traderequesthistoryitem=models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="traderequesthistoryitem")
+
+    notifications = GenericRelation(Notification) 
+
+    def __str__(self):
+        return self.user.username
+
+    def add_trade_history(self, other_trader):
+        if not other_trader in self.traderequesthistoryitem.all():
+            self.traderequesthistoryitem.add(other_trader)
+            content_type = ContentType.objects.get_for_model(self)
+
+
+            #Notification Creation
+            self.notifications.create(
+                target = self.user,
+                from_user = other_trader,
+                redirect_url = f"{settings.BASE_URL}/notifications.html/",
+                verb=f"Request successfull with {other_trader.username}",
+                content_type = content_type,
+            )    
+            self.save()
+
+
 
 class traderequest(models.Model):
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sender")
@@ -55,5 +87,66 @@ class traderequest(models.Model):
     def __str__(self):
         return self.sender
 
-    
+    def accept(self):
+        receiver_trade_list = TradeList.objects.get(user=self.receiver)
+        if(receiver_trade_list):
+            content_type = ContentType.objects.get_for_model(self)
+			# Update notification for RECEIVER
+            receiver_notification = Notification.objects.get(target=self.receiver, content_type=content_type, object_id=self.id)
+            receiver_notification.is_active = False
+            receiver_notification.redirect_url = f"{settings.BASE_URL}/notifications.html/"
+            receiver_notification.verb = f"You accepted {self.sender.username}'s Trade request."
+            #receiver_notification.timestamp = timezone.now()
+            receiver_notification.save()
+            receiver_trade_list.add_trade_history(self.sender)
+            # receiver_friend_list.save()
+            sender_trade_list = TradeList.objects.get(user=self.sender)
+            if sender_trade_list:
 
+				# Create notification for SENDER
+                self.notifications.create(
+					target=self.sender,
+					from_user=self.receiver,
+					redirect_url=f"{settings.BASE_URL}/notifications.html/",
+					verb=f"{self.receiver.username} accepted your Trade request.",
+					content_type=content_type,
+				)
+                sender_trade_list.add_trade_history(self.receiver)
+                # sender_friend_list.save()
+                self.is_active = False
+                self.save()
+            return receiver_notification
+			
+    def decline(self):
+        self.is_active = False
+        self.save()
+        content_type = ContentType.objects.get_for_model(self)
+        # Update notification for RECEIVER
+        notification = Notification.objects.get(target=self.receiver, content_type=content_type, object_id=self.id)
+        notification.is_active = False
+        notification.redirect_url = f"{settings.BASE_URL}/notifications.html/"
+        notification.verb = f"You declined {self.sender}'s Trade request."
+        notification.from_user = self.sender
+        #notification.timestamp = timezone.now()
+        notification.save()
+
+		# Create notification for SENDER
+        self.notifications.create(
+			target=self.sender,
+			verb=f"{self.receiver.username}'s Trade request declined.",
+			from_user=self.receiver,
+			redirect_url=f"{settings.BASE_URL}/notifications.html/",
+			content_type=content_type,
+        )
+        return notification
+        
+@receiver(post_save, sender=traderequest)
+def create_notification(sender, instance, created, **kwargs):
+	if created:
+		instance.notifications.create(
+			target=instance.receiver,
+			from_user=instance.sender,
+			redirect_url=f"{settings.BASE_URL}/notifications.html/",
+			verb=f"{instance.sender.username} sent you a Trade request.",
+			content_type=instance,
+		)
