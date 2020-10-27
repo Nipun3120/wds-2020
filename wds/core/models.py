e420e46a6e523850edec2c69ff205d8618ac0b39
Arjun Mittu <43925857+arjun-mittu@users.noreply.github.com>
2020-10-27 16:49:58 +0530

    accept trade request completed

--- wds/core/models.py
+++ wds/core/models.py
@@ -106,9 +106,6 @@ class FriendRequest(models.Model):
         self.is_active=False
         self.save()
 
-
-def trade_request(seller,buyer):
-    pass
 class tradereq(models.Model):
     sender=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sender_trade")
     receiver=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="receiver_trade")
@@ -119,10 +116,82 @@ class tradereq(models.Model):
     is_active= models.BooleanField(blank=False, null=False, default=True)
     def __str__(self):
         return self.sender.username
-
     def accept(self):
         receiver_stock=Stock.objects.get(user=self.receiver)
         sender_stock=Stock.objects.get(user=self.sender)
         amount=self.numberofstocks*self.priceperstock
+        if self.action=='buy':
+            sender_stock.userbalance=sender_stock.userbalance-amount
+            receiver_stock.userbalance=receiver_stock.userbalance+amount
+            if self.stock=='stock1':
+                sender_stock.stock1=sender_stock.stock1+self.numberofstocks
+                receiver_stock.stock1=receiver_stock.stock1-self.numberofstocks
+            elif self.stock=='stock2':
+                sender_stock.stock2=sender_stock.stock2+self.numberofstocks
+                receiver_stock.stock2=receiver_stock.stock2-self.numberofstocks
+            elif self.stock=='stock3':
+                sender_stock.stock3=sender_stock.stock3+self.numberofstocks
+                receiver_stock.stock3=receiver_stock.stock3-self.numberofstocks
+            elif self.stock=='stock4':
+                sender_stock.stock4=sender_stock.stock4+self.numberofstocks
+                receiver_stock.stock4=receiver_stock.stock4-self.numberofstocks
+            elif self.stock=='stock5':
+                sender_stock.stock5=sender_stock.stock5+self.numberofstocks
+                receiver_stock.stock5=receiver_stock.stock5-self.numberofstocks
+            elif self.stock=='stock6':
+                sender_stock.stock6=sender_stock.stock6+self.numberofstocks
+                receiver_stock.stock6=receiver_stock.stock6-self.numberofstocks
+            elif self.stock=='stock7':
+                sender_stock.stock7=sender_stock.stock7+self.numberofstocks
+                receiver_stock.stock7=receiver_stock.stock7-self.numberofstocks
+            elif self.stock=='stock8':
+                sender_stock.stock8=sender_stock.stock8+self.numberofstocks
+                receiver_stock.stock8=receiver_stock.stock8-self.numberofstocks
+            elif self.stock=='stock9':
+                sender_stock.stock9=sender_stock.stock9+self.numberofstocks
+                receiver_stock.stock9=receiver_stock.stock9-self.numberofstocks
+            elif self.stock=='stock10':
+                sender_stock.stock10=sender_stock.stock10+self.numberofstocks
+                receiver_stock.stock10=receiver_stock.stock10-self.numberofstocks
+            
+            receiver_stock.save()
+            sender_stock.save()
+        elif self.action=='sell':
+            sender_stock,receiver_stock=receiver_stock,sender_stock
+            sender_stock.userbalance=sender_stock.userbalance-amount
+            receiver_stock.userbalance=receiver_stock.userbalance+amount
+            if self.stock=='stock1':
+                sender_stock.stock1=sender_stock.stock1+self.numberofstocks
+                receiver_stock.stock1=receiver_stock.stock1-self.numberofstocks
+            elif self.stock=='stock2':
+                sender_stock.stock2=sender_stock.stock2+self.numberofstocks
+                receiver_stock.stock2=receiver_stock.stock2-self.numberofstocks
+            elif self.stock=='stock3':
+                sender_stock.stock3=sender_stock.stock3+self.numberofstocks
+                receiver_stock.stock3=receiver_stock.stock3-self.numberofstocks
+            elif self.stock=='stock4':
+                sender_stock.stock4=sender_stock.stock4+self.numberofstocks
+                receiver_stock.stock4=receiver_stock.stock4-self.numberofstocks
+            elif self.stock=='stock5':
+                sender_stock.stock5=sender_stock.stock5+self.numberofstocks
+                receiver_stock.stock5=receiver_stock.stock5-self.numberofstocks
+            elif self.stock=='stock6':
+                sender_stock.stock6=sender_stock.stock6+self.numberofstocks
+                receiver_stock.stock6=receiver_stock.stock6-self.numberofstocks
+            elif self.stock=='stock7':
+                sender_stock.stock7=sender_stock.stock7+self.numberofstocks
+                receiver_stock.stock7=receiver_stock.stock7-self.numberofstocks
+            elif self.stock=='stock8':
+                sender_stock.stock8=sender_stock.stock8+self.numberofstocks
+                receiver_stock.stock8=receiver_stock.stock8-self.numberofstocks
+            elif self.stock=='stock9':
+                sender_stock.stock9=sender_stock.stock9+self.numberofstocks
+                receiver_stock.stock9=receiver_stock.stock9-self.numberofstocks
+            elif self.stock=='stock10':
+                sender_stock.stock10=sender_stock.stock10+self.numberofstocks
+                receiver_stock.stock10=receiver_stock.stock10-self.numberofstocks
+            
+            receiver_stock.save()
+            sender_stock.save()
         self.is_active=False
         self.save()
